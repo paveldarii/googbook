0e16640cff6991971eafa0e2bfe3ab8336723940
Pavel Darii <paveldarii@yahoo.com>
2021-03-17 23:02:12 -0400

    created the basic structure of the home page:

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,15 +3,9 @@ import API from "../utils/API";
 import { Jumbotron, Button } from "react-bootstrap";
 
 function Home() {
-  // Setting our component's initial state
-  const [books, setBooks] = useState([]);
-
-  // Load all books and store them with setBooks
-  useEffect(() => {}, []);
-
   return (
     <Jumbotron>
-      <h1>Hello, world!</h1>
+      <h1>Searched</h1>
       <p>
         This is a simple hero unit, a simple jumbotron-style component for
         calling extra attention to featured content or information.
