4817af798eb05eb00c4d54a84129a1f2c194197a
Pavel Darii <paveldarii@yahoo.com>
2021-03-18 16:36:06 -0400

    changed the routes to coincide with front end routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const bookRoutes = require("./books");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/books/saved", bookRoutes);
 
 module.exports = router;
