92ea03711c69d75ac345d2ce6b1c8b7c13d86e0b
Pavel Darii <paveldarii@yahoo.com>
2021-03-18 15:17:00 -0400

    imported hero component

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,18 +1,7 @@
 import React from "react";
-import { Jumbotron, Button } from "react-bootstrap";
+import Hero from "../components/Hero";
 function NoMatch() {
-  return (
-    <Jumbotron>
-      <h1>Saved</h1>
-      <p>
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.
-      </p>
-      <p>
-        <Button variant="primary">Learn more</Button>
-      </p>
-    </Jumbotron>
-  );
+  return <Hero title="Saved Books" />;
 }
 
 export default NoMatch;
