cdb97aaec399b0fc1a99781e3f62d29f1b115053
Pavel Darii <paveldarii@yahoo.com>
2021-03-18 17:25:13 -0400

    created two functions that will be used in dependence weather the last button is save button or delete button in the bookCard component

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -1,25 +1,68 @@
 import React from "react";
 import { Image, Card, Button } from "react-bootstrap";
+import API from "../../utils/API";
 
 function BookCard(props) {
   const loadAuthors = (props) => {
-    if (props.book.authors) {
-      return <Card.Text>by {props.book.authors}</Card.Text>;
+    if (props.book.authors || props.authors) {
+      return <Card.Text>by {props.book.authors || props.authors}</Card.Text>;
     }
   };
+  function saveBook(bookData) {
+    API.saveBook(bookData)
+      .then((res) => {
+        alert("saved");
+      })
+      .catch((err) => console.log(err));
+  }
+  function deleteBook(_id) {
+    API.deleteSavedBook(_id)
+
+      .then((res) => {
+        alert("deleted");
+      })
+      .catch((err) => console.log(err));
+  }
+  function displayLastButton(props) {
+    switch (props.headerButton) {
+      case "delete": {
+        return (
+          <Button
+            onClick={() => {
+              console.log(props.book._id);
+              deleteBook(props.book._id);
+            }}
+            variant="outline-info"
+            style={{ background: "#66A5AD", float: "right" }}
+          >
+            Delete Book
+          </Button>
+        );
+      }
+      case "save": {
+        return (
+          <Button
+            onClick={() => {
+              console.log(props.book);
+              saveBook(props.book);
+            }}
+            variant="outline-info"
+            style={{ background: "#66A5AD", float: "right" }}
+          >
+            Save Book
+          </Button>
+        );
+      }
+    }
+  }
   return (
     <Card
-      key={props.book._id}
+      key={props.book.googleId}
       style={{ margin: "20px", background: "#C4DFE6" }}
     >
       <Card.Header style={{ background: "#07575B", color: "white" }}>
         {props.book.categories}
-        <Button
-          variant="outline-info"
-          style={{ background: "#66A5AD", float: "right" }}
-        >
-          Save Book
-        </Button>
+        {displayLastButton(props)}
         <Button
           href={props.book.link}
           variant="outline-info"
