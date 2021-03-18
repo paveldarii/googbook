c0f45429d3059648baa8cd211bbb53ec2fd11bda
Pavel Darii <paveldarii@yahoo.com>
2021-03-18 16:35:02 -0400

    added more state fields to the model book

--- models/book.js
+++ models/book.js
@@ -3,9 +3,13 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  authors: [{ type: String }],
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  link: { type: String, required: true },
+  categories: [{ type: String }],
+  googleId: { type: String, required: true },
+  date: { type: Date, default: Date.now },
 });
 
 const Book = mongoose.model("Book", bookSchema);
