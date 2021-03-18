3d2dbd7eb3c64b399a00e92a7207d7e6ff700e44
Pavel Darii <paveldarii@yahoo.com>
2021-03-18 16:37:47 -0400

    created most of the functionality of the saved books page

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Hero from "../components/Hero";
-function NoMatch() {
-  return <Hero title="Saved Books" />;
+import API from "../utils/API";
+import BookCard from "../components/BookCard";
+function Saved() {
+  const [books, setBooks] = useState({});
+
+  useEffect(() => {
+    loadBooks();
+  }, []);
+  function loadBooks() {
+    API.getSavedBooks()
+      .then((res) => {
+        console.log(res.data);
+        setBooks(res.data);
+      })
+      .catch((err) => console.log(err));
+  }
+  return (
+    <div>
+      <Hero title="Saved Books" />
+      {books.length ? (
+        <div>
+          {books.map((book) => (
+            <BookCard book={book} />
+          ))}
+        </div>
+      ) : (
+        <h3>No Results to Display</h3>
+      )}
+    </div>
+  );
 }
 
-export default NoMatch;
+export default Saved;
