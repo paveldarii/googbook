8ee412595b6c7a99e80f9f95938444704c996a9d
Pavel Darii <paveldarii@yahoo.com>
2021-03-18 13:08:29 -0400

    added most of the api crud routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,19 +2,24 @@ import axios from "axios";
 
 export default {
   // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  getBooks: function (query) {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
   },
   // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  getBook: function (id) {
+    return axios.get("/api/books/saved" + id);
   },
   // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+  deleteBook: function (id) {
+    return axios.delete("/api/books/saved" + id);
   },
   // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  }
+  saveBook: function (bookData) {
+    return axios.post("/api/books/saved", bookData);
+  },
+
+  //Get saved books
+  savedBooks: function () {
+    return axios.get("/api/books/saved");
+  },
 };
