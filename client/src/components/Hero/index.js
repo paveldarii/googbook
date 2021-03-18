b889a124492679b149cf9df1f88d30ef47090d43
Pavel Darii <paveldarii@yahoo.com>
2021-03-18 15:19:22 -0400

    created full functionality for the hero component

---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+
+function Hero(props) {
+  return (
+    <Jumbotron
+      style={{
+        backgroundImage:
+          "url('https://content.fortune.com/wp-content/uploads/2016/06/171152625.jpg')",
+        color: "Black",
+      }}
+    >
+      <h1
+        style={{
+          background: "#C4DFE6",
+          display: "inline",
+          padding: "0px 10px 0px 10px",
+          border: "3px solid #07575B",
+          borderRadius: "10px",
+        }}
+      >
+        {props.title}
+      </h1>
+    </Jumbotron>
+  );
+}
+
+export default Hero;
