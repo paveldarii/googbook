11b5e8feb542927b499aa1d27c14708156a0b2a7
Pavel Darii <paveldarii@yahoo.com>
2021-03-18 13:10:08 -0400

    added functionality so on every load of home page, it will make an api call to retrieve the books with the name stored in the localStorage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,31 @@ import API from "../utils/API";
 import { Jumbotron, Button } from "react-bootstrap";
 
 function Home() {
+  const [books, setBooks] = useState({});
+  const structureBook = (bookData) => {
+    return {
+      _id: bookData.id,
+      title: bookData.volumeInfo.title,
+      authors: bookData.volumeInfo.authors,
+      description: bookData.volumeInfo.description,
+      image: bookData.volumeInfo.imageLinks.thumbnail,
+      link: bookData.volumeInfo.previewLink,
+    };
+  };
+  useEffect(() => {
+    if (localStorage.getItem("searchQuery")) {
+      loadBooks();
+    }
+  }, []);
+  function loadBooks() {
+    API.getBooks(localStorage.getItem("searchQuery"))
+      .then((res) => {
+        console.log(res.data.items.map((bookData) => structureBook(bookData)));
+        setBooks(res.data.items.map((bookData) => structureBook(bookData)));
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <Jumbotron>
       <h1>Searched</h1>
