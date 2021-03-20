15e67c57de84920389dad1b8426f08f9b7b6a75b
Pavel Darii <paveldarii@yahoo.com>
2021-03-20 17:33:18 -0400

    changed the model validation so it will not be required to have all fields filled if the data don't exist

--- models/book.js
+++ models/book.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
+  title: { type: String },
   authors: [{ type: String }],
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-  link: { type: String, required: true },
+  description: { type: String },
+  image: { type: String },
+  link: { type: String },
   categories: [{ type: String }],
-  googleId: { type: String, required: true },
+  googleId: { type: String },
   date: { type: Date, default: Date.now },
 });
 
